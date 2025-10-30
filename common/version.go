590c779be328da5ccb15bac33b701e88a17936b4
Junlong Zhang <junlongzzz@qq.com>
2025-10-30 22:05:24 +0800

    Bump version to 2.0.0

--- common/version.go
+++ common/version.go
@@ -4,9 +4,9 @@ import "fmt"
 
 // 语义化的版本号 Semantic Versioning
 const (
-	versionX byte = 1
-	versionY byte = 3
-	versionZ byte = 5
+	versionX byte = 2
+	versionY byte = 0
+	versionZ byte = 0
 )
 
 // Version 获取 x.y.z 文本格式版本号
